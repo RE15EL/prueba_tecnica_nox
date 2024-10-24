006422199bdb139c569be14a28477c24a90bab09
Reisel Valle <reiselvalle@gmail.com>
2024-10-24 12:03:58 -0400

    cleaning

--- src/app/(base)/page.tsx
+++ src/app/(base)/page.tsx
@@ -1,32 +1,7 @@
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
-
 export default function Home() {
   return (
     <div className="p-4 pt-2 md:p-[26px] md:pt-[16px] lg:p-[52px] lg:pt-[36px] lg:px-[20px] h-full flex flex-col">
       <h2>Home page</h2>
-      <Select>
-      <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder="Select a fruit" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>Fruits</SelectLabel>
-          <SelectItem value="apple">Apple</SelectItem>
-          <SelectItem value="banana">Banana</SelectItem>
-          <SelectItem value="blueberry">Blueberry</SelectItem>
-          <SelectItem value="grapes">Grapes</SelectItem>
-          <SelectItem value="pineapple">Pineapple</SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
     </div>
   );
 }
